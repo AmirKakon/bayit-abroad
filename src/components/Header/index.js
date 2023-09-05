19e4351cf87e9db116c52566a38faa0c780a106f
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-05 15:04:55 +0300

    header fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,13 +6,11 @@ const Header = () => {
     return (
         <header>
             <Box sx={{ bgcolor: "#2c3c30", color: "white", height: "70", px: 1 }}>
-                <Grid container alignItems="center" justifyContent="space-between">
+                <Grid container alignItems="center" justifyContent="start">
                     <Link to="/home" sx={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
                         <img src={logo} alt="Bayit Abroad Logo" height={70} />
                     </Link>
-                    <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
-                        <Typography variant="h4">Welcome to Bayit Abroad</Typography>
-                    </Box>
+                        <Typography variant="h5">Bayit Abroad</Typography>
                 </Grid>
             </Box>
         </header>

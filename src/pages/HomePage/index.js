92371ea2318a058bad4b39cf8259a97bca64dae4
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-05 15:03:27 +0300

    circles centered

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -4,11 +4,17 @@ import { Box, Grid } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <Box sx={{ bgcolor: "#f0f0f0", color: "black", height: "100%", px: 1 }}>
-      <Grid container alignItems="center" justifyContent="space-between">
-        <CircleIcon size="100" />
-        <CircleIcon size="100" />
-        <CircleIcon size="100" />
+    <Box sx={{ bgcolor: "#f0f0f0", color: "black", height: "100%", px: "15%" }}>
+      <Grid container spacing={2} justifyContent="space-between" sx={{ margin: "auto" }}>
+        <Grid item>
+          <CircleIcon size="150" />
+        </Grid>
+        <Grid item>
+          <CircleIcon size="150" />
+        </Grid>
+        <Grid item>
+          <CircleIcon size="150" />
+        </Grid>
       </Grid>
     </Box>
   );

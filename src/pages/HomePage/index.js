f185a51e18eff0785873b0ccd019f3ce92905526
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-07 10:53:19 +0300

    how it works

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -93,6 +93,38 @@ const whyChooseUs = (
   </Grid>
 );
 
+const howItWorks = (
+  <Grid container direction="column" spacing={2} alignItems="center">
+    <Grid item>
+      <Typography variant="h4" fontFamily="serif" align="center" color="black">
+        HOW IT WORKS
+      </Typography>
+      <br />
+    </Grid>
+    <Grid item sx={{ textAlign: "center" }}>
+      <Typography variant="h6" fontFamily="serif" align="center" color="black">
+        Start by choosing the items you need from the order form below, whether
+        it be platas, hot water urns or Shabbat candles.
+        <br />
+        <br />
+        Once you place your order and delivery dates we’ll take care of the
+        rest.
+        <br />
+        <br />
+        Our experienced delivery team will deliver and pick up the items
+        straight to your door*
+        <br />
+        <br />
+        <Typography variant="body1" fontFamily="serif" align="center" color="black" component="span" fontStyle="italic">
+        *Delivery and pickup are limited to the city of Jerusalem. For requests
+        outside of Jerusalem please contact us by email or Whatsapp
+        </Typography>
+      </Typography>
+      <br />
+    </Grid>
+  </Grid>
+);
+
 const HomePage = () => {
   return (
     <Box
@@ -107,6 +139,8 @@ const HomePage = () => {
       {header}
       <Divider variant="middle" sx={{ marginTop: 3, marginBottom: 3 }} />
       {whyChooseUs}
+      <Divider variant="middle" sx={{ marginTop: 3, marginBottom: 3 }} />
+      {howItWorks}
     </Box>
   );
 };

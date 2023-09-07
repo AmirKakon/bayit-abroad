b79bc15fbc1d30c8297b37caf1e96b9a0948547e
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-07 14:35:09 +0300

    test domain 1

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -14,7 +14,7 @@ const Footer = () => {
             &copy; 2023 All Rights Reserved, Bayit Abroad
           </Typography>
           <Typography color="textSecondary" fontFamily="serif">
-            version 1.0.0
+            version 2.0.0
           </Typography>
         </Grid>
 

075729bc7fa5ee73f2afcc4e1dcd777362932d0b
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-10 12:20:39 +0300

    form header

--- src/pages/FormPage/index.js
+++ src/pages/FormPage/index.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
-import { Button, Container, TextField, Typography, FormControl, InputLabel, Select, MenuItem, FormControlLabel, Checkbox } from '@mui/material';
-import { Box } from '@mui/system';
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControlLabel,
+  Checkbox,
+  Paper,
+} from "@mui/material";
+import { Box } from "@mui/system";
 
 const FormPage = () => {
-    const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
 
-    const items = ['Item 1', 'Item 2', 'Item 3']; // Example items
-  
-    const handleCheckboxChange = (event) => {
-      if (event.target.checked) {
-        setSelectedItems(prev => [...prev, event.target.name]);
-      } else {
-        setSelectedItems(prev => prev.filter(item => item !== event.target.name));
-      }
-    };
+  const items = ["Item 1", "Item 2", "Item 3"]; // Example items
+
+  const handleCheckboxChange = (event) => {
+    if (event.target.checked) {
+      setSelectedItems((prev) => [...prev, event.target.name]);
+    } else {
+      setSelectedItems((prev) =>
+        prev.filter((item) => item !== event.target.name)
+      );
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -22,74 +36,99 @@ const FormPage = () => {
   };
 
   return (
-    <Box flex={1}>
-    <Container component="main" maxWidth="md">
-      <Typography variant="h5" align="center" gutterBottom>
-        Bayit Abroad Order Form
-      </Typography>
-      <Typography variant="body1" align="center" paragraph>
-        Please fill out this form for your order.
-      </Typography>
-      
-      <form onSubmit={handleSubmit}>
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          label="Full Name"
-          required
-        />
+    <Box
+      flex={1}
+      sx={{
+        minHeight: "200",
+        padding: 2, // Consistent padding
+      }}
+    >
+      <Container component="main" maxWidth="md">
+          <Typography variant="h4" align="center" gutterBottom>
+            Bayit Abroad Order Form
+          </Typography>
+          <Typography variant="body1" align="center" paragraph>
+            Thanks for choosing to order from us!
+          </Typography>
+          <Paper elevation={2} sx={{ padding: 1, marginBottom: 5}}>
+          <Typography variant="body1" align="left" paragraph>
+            Please fill out which equipment you'd like to rent, the weekend of
+            the rental and the location.
+            <br />
+            <br />
+            Looking for an item that isn't listed below? Add more items in the
+            "Additional Notes" section at the bottom of the form and our team
+            will review the request and let you know if we can supply it for
+            you.
+            <br />
+            <br />
+            * Please note that delivery is only in Jerusalem. Drop off is
+            on Thursday night / Friday morning and pick it up is on Saturday
+            night / Sunday morning, depending on our availability and your
+            preference.
+          </Typography>
+        </Paper>
+        <form onSubmit={handleSubmit}>
+          <Paper elevation={2} sx={{ padding: 1 }}>
+            <FormControl component="fieldset">
+              <Typography component="legend">Select Items:</Typography>
+              {items.map((item) => (
+                <FormControlLabel
+                  key={item}
+                  control={
+                    <Checkbox
+                      name={item}
+                      checked={selectedItems.includes(item)}
+                      onChange={handleCheckboxChange}
+                      color="primary"
+                    />
+                  }
+                  label={item}
+                />
+              ))}
+            </FormControl>
+          </Paper>
 
-        <TextField
-          fullWidth
-          variant="outlined"
-          margin="normal"
-          label="Email Address"
-          type="email"
-          required
-        />
-
-        {/* You can continue adding more fields like this. For dropdowns, use Select: */}
-        <FormControl fullWidth variant="outlined" margin="normal">
-          <InputLabel>Some Select Field</InputLabel>
-          <Select>
-            <MenuItem value={10}>Option 1</MenuItem>
-            <MenuItem value={20}>Option 2</MenuItem>
-            <MenuItem value={30}>Option 3</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl component="fieldset" margin="normal">
-        <Typography component="legend">Select Items:</Typography>
-        {items.map(item => (
-          <FormControlLabel
-            key={item}
-            control={
-              <Checkbox
-                name={item}
-                checked={selectedItems.includes(item)}
-                onChange={handleCheckboxChange}
-                color="primary"
-              />
-            }
-            label={item}
+          <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            label="Full Name"
+            required
           />
-        ))}
-      </FormControl>
 
-        {/* ... Add other fields ... */}
+          <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            label="Email Address"
+            type="email"
+            required
+          />
 
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          style={{ marginTop: '16px' }}
-        >
-          Submit
-        </Button>
-      </form>
-    </Container>
+          {/* You can continue adding more fields like this. For dropdowns, use Select: */}
+          <FormControl fullWidth variant="outlined" margin="normal">
+            <InputLabel>Some Select Field</InputLabel>
+            <Select>
+              <MenuItem value={10}>Option 1</MenuItem>
+              <MenuItem value={20}>Option 2</MenuItem>
+              <MenuItem value={30}>Option 3</MenuItem>
+            </Select>
+          </FormControl>
+
+          {/* ... Add other fields ... */}
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            style={{ marginTop: "16px" }}
+          >
+            Submit
+          </Button>
+        </form>
+      </Container>
     </Box>
   );
 };
--- src/theme.js
+++ src/theme.js
@@ -1,37 +1,38 @@
 export const lightThemeOptions = {
-    palette: {
-      mode: 'light',
-      primary: {
-        main: '#2c3c30',
-      },
-      secondary: {
-        main: '#e6deca',
-      },
-      background: {
-        default: '#e6e6e6',
-      },
-      text: {
-        disabled: '#9e9e9e',
-        secondary: '#2c3c30',
-        primary: '#000000',
-      },
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#2c3c30",
     },
-    typography: {
-      fontFamily: 'Droid Serif',
+    secondary: {
+      main: "#e6deca",
     },
-  };
+    background: {
+      default: "#e6e6e6",
+      paper: "#e1e1e1",
+    },
+    text: {
+      disabled: "#9e9e9e",
+      secondary: "#2c3c30",
+      primary: "#000000",
+    },
+  },
+  typography: {
+    fontFamily: "Droid Serif",
+  },
+};
 
-  export const darkThemeOptions = {
-    palette: {
-      mode: 'dark',
-      primary: {
-        main: '#e6deca',
-      },
-      secondary: {
-        main: '#2c3c30',
-      },
+export const darkThemeOptions = {
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#e6deca",
     },
-    typography: {
-      fontFamily: 'Droid Serif',
+    secondary: {
+      main: "#2c3c30",
     },
-  };
+  },
+  typography: {
+    fontFamily: "Droid Serif",
+  },
+};

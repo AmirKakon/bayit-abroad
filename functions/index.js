e1abb1549c691e4d117b04f29d7287856085174d
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-11 17:40:05 +0300

    getItem endpoint backend

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,25 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
 const { logger } = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
-const { onDocumentCreated } = require("firebase-functions/v2/firestore");
 
 // The Firebase Admin SDK to access Firestore.
 const { initializeApp } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
 initializeApp();
+
+const db = getFirestore();
+
+exports.getItem = onRequest(async (req, res) => {
+  const itemId = req.query.itemId;
+  const itemRef = db.collection("items").doc(itemId);
+
+  const doc = await itemRef.get();
+  if (!doc.exists) {
+    logger.log("Error - No data found");
+  } else {
+    logger.log("Document:", doc.data());
+  }
+  // Send back a message that we've successfully written the message
+  res.json(doc.data());
+});

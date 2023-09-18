ff692fb48bc2147b795329172dd6b812d1d86108
Amir Kakon <amir.kakon@mesh-systems.com>
2023-09-18 16:41:51 +0300

    getAllGames cloud functions

--- functions/index.js
+++ functions/index.js
@@ -65,3 +65,31 @@ exports.getAllItems = onRequest(async (req, res) => {
     }
   });
 });
+
+const gamesId = "BhT9GsyGCCs7OsmklyJz";
+
+exports.getAllGames = onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    try {
+      const itemsRef = db.collection("items").doc(gamesId).collection("games");
+      const snapshot = await itemsRef.get();
+
+      if (snapshot.empty) {
+        logger.log("No data found");
+        res.status(404).send("No data found");
+        return;
+      }
+
+      const items = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      logger.log("Games List", items);
+      res.status(200).json(items);
+    } catch (error) {
+      logger.error("Error fetching data: ", error);
+      res.status(500).send("Error fetching data");
+    }
+  });
+});

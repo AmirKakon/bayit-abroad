583dbb6b344e0914b5b2c4a85549256add33285f
Amir Kakon <amir.kakon@mesh-systems.com>
2023-10-02 14:27:07 +0300

    global get and post userDetails

--- functions/index.js
+++ functions/index.js
@@ -1,95 +1,66 @@
-const cors = require("cors")({ origin: true });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const { logger } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/v2/https");
+var serviceAccount = require("./serviceAccountKey.json");
 
-// The Firebase Admin SDK to access Firestore.
-const { initializeApp } = require("firebase-admin/app");
-const { getFirestore } = require("firebase-admin/firestore");
-
-initializeApp();
-
-const db = getFirestore();
-
-exports.getItem = onRequest(async (req, res) => {
-  cors(req, res, async () => {
-    try {
-      const itemId = req.query.itemId;
-      const itemRef = db.collection("items").doc(itemId);
-      const doc = await itemRef.get();
-      if (!doc.exists) {
-        logger.log("Error - No data found");
-        res.status(404).send("No data found");
-      } else {
-        logger.log("Item:", doc.data());
-        res.json(doc.data());
-      }
-    } catch (error) {
-      logger.error("Error fetching item: ", error);
-      res.status(500).send("Error fetching item");
-    }
-  });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bayitabroad-jkak-default-rtdb.firebaseio.com",
 });
 
-const preferredId = "myszTIFQHloPy7Xksgw2"; // 'entire package' id
+const express = require("express");
+const cors = require("cors");
 
-exports.getAllItems = onRequest(async (req, res) => {
-  cors(req, res, async () => {
-    try {
-      const itemsRef = db.collection("items");
-      const snapshot = await itemsRef.get();
+//Main App
+const app = express();
+app.use(cors({ orgin: true }));
 
-      if (snapshot.empty) {
-        logger.log("No data found");
-        res.status(404).send("No data found");
-        return;
-      }
+//Main database reference
+const db = admin.firestore();
 
-      let items = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      const preferredItem = items.find((item) => item.id === preferredId);
-      if (preferredItem) {
-        items = items.filter((item) => item.id !== preferredId);
-        items.unshift(preferredItem);
-      }
-
-      logger.log("Item List", items);
-      res.status(200).json(items);
-    } catch (error) {
-      logger.error("Error fetching data: ", error);
-      res.status(500).send("Error fetching data");
-    }
-  });
+//Routes
+app.get("/", (req, res) => {
+  return res.status(200).send("Root Get Call");
 });
 
-const gamesId = "BhT9GsyGCCs7OsmklyJz";
-
-exports.getAllGames = onRequest(async (req, res) => {
-  cors(req, res, async () => {
+//create -> post()
+app.post("/api/create", (req, res) => {
+  // async waits for a response
+  (async () => {
     try {
-      const itemsRef = db.collection("items").doc(gamesId).collection("games");
-      const snapshot = await itemsRef.get();
+      //await is the pair of async to tell the function which line to wait until completed.
+      await db.collection("userDetails").doc(`/${Date.now()}/`).create({
+        id: Date.now(),
+        name: req.body.name,
+        mobile: req.body.mobile,
+      });
 
-      if (snapshot.empty) {
-        logger.log("No data found");
-        res.status(404).send("No data found");
-        return;
-      }
-
-      const items = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-
-      logger.log("Games List", items);
-      res.status(200).json(items);
+      return res.status(200).send({ status: "Sucess", msg: "Data Saved" });
     } catch (error) {
-      logger.error("Error fetching data: ", error);
-      res.status(500).send("Error fetching data");
+      console.log(error);
+      return res.status(500).send({ status: "Failed", msg: error });
     }
-  });
+  })();
 });
+
+//get -> get()
+//fetch single data from firestore using specific id
+app.get("/api/get/:id", (req, res) => {
+  (async () => {
+    try {
+      const reqDoc = 
+
+      return res.status(200).send({ status: "Sucess", msg: "Data Saved" });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ status: "Failed", msg: error });
+    }
+  })();
+});
+
+//update -> put()
+
+//delete -> delete()
+
+//exports the api to firebase functions
+exports.app = functions.https.onRequest(app);

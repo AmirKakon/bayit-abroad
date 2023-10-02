4c4457524f20cc9afde2700db05c28e4e789f8ed
Amir Kakon <amir.kakon@mesh-systems.com>
2023-10-02 15:00:47 +0300

    getall users

--- functions/index.js
+++ functions/index.js
@@ -48,9 +48,39 @@ app.post("/api/create", (req, res) => {
 app.get("/api/get/:id", (req, res) => {
   (async () => {
     try {
-      const reqDoc =db.collection("userDetails").doc(req.params.id); //reference
+      const reqDoc = db.collection("userDetails").doc(req.params.id); //reference
       let userDetail = await reqDoc.get(); //gets doc
       let response = userDetail.data(); //the actual data of the user
+
+      return res.status(200).send({ status: "Sucess", data: response });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ status: "Failed", msg: error });
+    }
+  })();
+});
+
+//fetch all user data from firestore
+app.get("/api/getAll", (req, res) => {
+  (async () => {
+    try {
+      const query = db.collection("userDetails");
+      let response = [];
+
+      await query.get().then((data) => {
+        let docs = data.docs;
+
+        docs.map((doc) => {
+          const selectedItem = {
+            id: doc.data().id,
+            name: doc.data().name,
+            mobile: doc.data().mobile,
+          };
+
+          response.push(selectedItem);
+        });
+        return response
+      });
 
       return res.status(200).send({ status: "Sucess", data: response });
     } catch (error) {

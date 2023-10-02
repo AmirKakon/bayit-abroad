f95fa7831fe79c62e95e348ef26a577a130c8137
Amir Kakon <amir.kakon@mesh-systems.com>
2023-10-02 15:31:50 +0300

    tutorial complete

---
+++ functions/setup.js
@@ -0,0 +1,18 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bayitabroad-jkak-default-rtdb.firebaseio.com",
+});
+
+const app = express();
+app.use(cors({ orgin: true }));
+
+const db = admin.firestore();
+
+module.exports = { app, functions, db };

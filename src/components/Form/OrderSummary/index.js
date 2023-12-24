0c647842e17583e09b66a571eaf089cf7e1ca1c0
Amir Kakon <amir.kakon@mesh-systems.com>
2023-12-24 17:41:34 +0200

    summary complete

--- src/components/Form/OrderSummary/index.js
+++ src/components/Form/OrderSummary/index.js
@@ -9,118 +9,119 @@ import {
   Paper,
   Typography,
 } from '@mui/material';
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
-const OrderSummary = ({ order, selectedItemsList, totalAmount }) => {
-  const setDateString = (date) => {
-    // Implement your date formatting logic here
-    return date.toLocaleDateString();
-  };
+const InformationSection = ({ order }) => {
+  return (
+    <>
+    <TableHead>
+      <TableRow>
+        <TableCell colSpan={2} style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
+          <Typography variant="body1" sx={{ marginLeft: 1, fontWeight: 'bold' }}>
+            Information
+          </Typography>
+        </TableCell>
+      </TableRow>
+      </TableHead>
+      <TableBody>
+        {[
+          ['Name:', order.fullName],
+          ['Email:', order.email],
+          ['Phone:', order.phoneNumber],
+          ['Address:', order.deliveryAddress],
+          ['Delivery Date:', dayjs(order.dateRange.delivery).format('ddd MMM D YYYY')],
+          ['Pickup Date:', dayjs(order.dateRange.pickup).format('ddd MMM D YYYY')],
+          ['Additional Notes:', order.additionalNotes],
+        ].map((row) => (
+          <TableRow key={row[0]}>
+            <TableCell width="10%">{row[0]}</TableCell>
+            <TableCell>{row[1]}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      </>
+  );
+};
 
-  const itemList = order.selectedItems.map((item) => {
-    return <TableRow>
-    <TableCell width="30%">
-      {item.name}
-    </TableCell>
-    <TableCell width="10%">${item.price.usd}</TableCell>
-    <TableCell>₪{item.price.nis}</TableCell>
-    <TableCell>{item.amount}</TableCell>
-  </TableRow>
-  });
+const ItemsSection = ({ items, totalPrice }) => {
+    let totalAmountOfItems = 0;
 
+  const itemList = items.map((item) => {
+    totalAmountOfItems += item.amount;
+
+    return <TableRow key={item.name}>
+      <TableCell width="40%">{item.name}</TableCell>
+      <TableCell width="10%">${item.price.usd}</TableCell>
+      <TableCell>₪{item.price.nis}</TableCell>
+      <TableCell>{item.amount}</TableCell>
+      <TableCell width="10%">${item.amount*item.price.usd}</TableCell>
+      <TableCell>₪{item.amount*item.price.nis}</TableCell>
+    </TableRow>
+});
+
+  return (
+    <>
+    <TableHead>
+      <TableRow>
+        <TableCell style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
+          <Typography variant="body1" sx={{ marginLeft: 1, fontWeight: 'bold' }}>
+            Items
+          </Typography>
+        </TableCell>
+        <TableCell colSpan={2} style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
+          <Typography variant="body1" sx={{ marginLeft: 2, fontWeight: 'bold' }}>
+            Price Per Item
+          </Typography>
+        </TableCell>
+        <TableCell style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
+          <Typography variant="body1" sx={{ paddingRight: 3, fontWeight: 'bold' }}>
+            Amount
+          </Typography>
+        </TableCell>
+        <TableCell colSpan={2} style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
+          <Typography variant="body1" sx={{ marginLeft: 3, fontWeight: 'bold' }}>
+            Total Price
+          </Typography>
+        </TableCell>
+      </TableRow>
+      </TableHead>
+      <TableBody>
+        {itemList}
+        <TableRow>
+          <TableCell colSpan={3}>
+            <strong>Total:</strong>
+          </TableCell>
+          <TableCell>{totalAmountOfItems}</TableCell>
+          <TableCell>${totalPrice.usd}</TableCell>
+          <TableCell>₪{totalPrice.nis}</TableCell>
+        </TableRow>
+      </TableBody>
+      </>
+  );
+};
+
+const OrderSummary = ({ order }) => {
   return (
     <div>
       <TableContainer component={Paper}>
-        <Table size='small'>
+        <Table size="small">
           <TableHead>
             <TableRow>
               <TableCell colSpan={2} style={{ backgroundColor: '#2c3c30', padding: '10px' }}>
-              <Typography variant="h5" color="#e6deca" sx={{ marginLeft: 1 }}>Order Summary</Typography>
+                <Typography variant="h5" color="#e6deca" sx={{ marginLeft: 1 }}>
+                  Order Summary
+                </Typography>
               </TableCell>
             </TableRow>
           </TableHead>
 
-
-          <TableHead>
-            <TableRow>
-              <TableCell colSpan={2} style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
-              <Typography variant="body1" sx={{ marginLeft: 1, fontWeight: "bold" }}>Information</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-
-          <TableBody>
-            <TableRow>
-              <TableCell width="10%">
-                Name:
-              </TableCell>
-              <TableCell >{order.fullName}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell >
-                Email:
-              </TableCell>
-              <TableCell >{order.email}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell >
-                Phone:
-              </TableCell>
-              <TableCell >{order.phoneNumber}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell >
-                Address:
-              </TableCell>
-              <TableCell >{order.deliveryAddress}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell >
-                Delivery Date:
-              </TableCell>
-              <TableCell>{dayjs(order.dateRange.delivery).format("ddd MMM D YYYY")}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>
-                Pickup Date:
-              </TableCell>
-              <TableCell >{dayjs(order.dateRange.pickup).format("ddd MMM D YYYY")}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell >
-               Additional Notes:
-              </TableCell>
-              <TableCell >{order.additionalNotes}</TableCell>
-            </TableRow>
-          </TableBody>
-          </Table>
+          <InformationSection order={order} />
+        </Table>
       </TableContainer>
 
       <TableContainer component={Paper}>
-        <Table size='small'>
-          <TableHead>
-            <TableRow>
-              <TableCell  style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
-              <Typography variant="body1" sx={{ marginLeft: 1, fontWeight: "bold" }}>Items</Typography>
-              </TableCell>
-              <TableCell colSpan={2} style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
-              <Typography variant="body1" sx={{ marginLeft: 5, fontWeight: "bold" }}>Price</Typography>
-              </TableCell>
-              <TableCell style={{ backgroundColor: '#d5d5d5', padding: '5px' }}>
-              <Typography variant="body1" sx={{ fontWeight: "bold" }}>Amount</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {itemList}
-            <TableRow>
-              <TableCell >
-               <strong>Total:</strong>
-              </TableCell>
-              <TableCell >${order.totalPrice.usd}</TableCell>
-              <TableCell>₪{order.totalPrice.nis}</TableCell>
-            </TableRow>
-          </TableBody>
+        <Table size="small">
+          <ItemsSection items={order.selectedItems} totalPrice={order.totalPrice} />
         </Table>
       </TableContainer>
     </div>

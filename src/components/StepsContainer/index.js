69739310c5df263b323d9a92fb64b3875551dcff
Amir Kakon <amir.kakon@mesh-systems.com>
2023-12-25 14:24:04 +0200

    change steps and default date

--- src/components/StepsContainer/index.js
+++ src/components/StepsContainer/index.js
@@ -39,22 +39,11 @@ const StepsContainer = ({
           </Step>
         ))}
       </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Box sx={{ flex: "1 1 auto" }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
         <React.Fragment>
           {renderStep(activeStep)}
 
           <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Button
+            {activeStep > 0 ? (<Button
               variant="contained"
               color="primary"
               disabled={activeStep === 0}
@@ -62,7 +51,7 @@ const StepsContainer = ({
               sx={{ mr: 1 }}
             >
               Back
-            </Button>
+            </Button>) : null}
 
             <Box sx={{ flex: "1 1 auto" }} />
 
@@ -77,7 +66,6 @@ const StepsContainer = ({
             </Button>
           </Box>
         </React.Fragment>
-      )}
     </Box>
   );
 };

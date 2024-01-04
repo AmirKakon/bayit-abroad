de60b428d41bd5d76ed7f0c3eb60b6ff3c52be2f
Amir Kakon <amir.kakon@mesh-systems.com>
2024-01-04 09:40:44 +0200

    week calculation fixed

--- src/components/Form/OrderSummary/index.js
+++ src/components/Form/OrderSummary/index.js
@@ -14,6 +14,7 @@ import dayjs from "dayjs";
 const InformationSection = ({ order, thankyou }) => {
   const rows = [
     thankyou ? ["Tracking Number:", order.id] : null,
+    ["Status:", order.status],
     ["Name:", order.fullName],
     ["Email:", order.email],
     ["Phone in Israel:", order.phone],
--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const gamesId = "1tRC1jxs6fRXCA69eIal";
 export const formEntireItemsId = "XkfGiR95lXrZveSxToMl";
+
+export const orderStatus = [ "Processing", "Approved", "Ready for Shipment" ];

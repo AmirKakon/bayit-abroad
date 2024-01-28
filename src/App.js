d36458cba70b82d5f05e7eeb6b8a48cb0e02ed30
Amir Kakon <amir.kakon@mesh-systems.com>
2024-01-28 10:57:54 +0200

    Update environment variables and fix formatting

--- src/App.js
+++ src/App.js
@@ -1,45 +1,72 @@
-import React, {useEffect} from 'react';
-import ReactGA from 'react-ga';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Header from "./layout/Header";
-import {HomePage, FormPage, ThankYouPage, SearchOrderPage, AboutPage} from './pages';
-import Footer from './layout/Footer';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { lightThemeOptions } from './theme';
-import { Box } from '@mui/system';
-import { useMediaQuery } from '@mui/material';
+import {
+  HomePage,
+  FormPage,
+  ThankYouPage,
+  SearchOrderPage,
+  AboutPage,
+} from "./pages";
+import Footer from "./layout/Footer";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { lightThemeOptions } from "./theme";
+import { Box } from "@mui/system";
+import { useMediaQuery } from "@mui/material";
 
 const theme = createTheme(lightThemeOptions);
 
 const App = () => {
-  const isSmallScreen = useMediaQuery('(max-width:600px)');
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
 
   useEffect(() => {
-    ReactGA.initialize('G-70SKMMYSNZ');
+    const trackingId = process.env.REACT_APP_GA_TRACKING_ID;
+    ReactGA.initialize(`${trackingId}`);
     ReactGA.pageview(window.location.pathname + window.location.search);
     console.log(window.location.pathname + window.location.search);
   }, []);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Header isSmallScreen={isSmallScreen}/>
-        <Box display="flex" flexDirection="column" minHeight="100vh">
-          <Routes>
-            <Route path="/home" element={<HomePage isSmallScreen={isSmallScreen}/>} />
-            <Route path="/form" element={<FormPage isSmallScreen={isSmallScreen}/>} />
-            <Route path="/orders/:id/thankyou" element={<ThankYouPage isSmallScreen={isSmallScreen}/>} />
-            <Route path="/orders/search" element={<SearchOrderPage isSmallScreen={isSmallScreen}/>} />
-            <Route path="/about" element={<AboutPage isSmallScreen={isSmallScreen}/>} />
-            <Route path="*" element={<Navigate to="/home" replace />} />
-          </Routes>
-          <Footer isSmallScreen={isSmallScreen} />
-        </Box>
-      </Router>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Header isSmallScreen={isSmallScreen} />
+          <Box display="flex" flexDirection="column" minHeight="100vh">
+            <Routes>
+              <Route
+                path="/home"
+                element={<HomePage isSmallScreen={isSmallScreen} />}
+              />
+              <Route
+                path="/form"
+                element={<FormPage isSmallScreen={isSmallScreen} />}
+              />
+              <Route
+                path="/orders/:id/thankyou"
+                element={<ThankYouPage isSmallScreen={isSmallScreen} />}
+              />
+              <Route
+                path="/orders/search"
+                element={<SearchOrderPage isSmallScreen={isSmallScreen} />}
+              />
+              <Route
+                path="/about"
+                element={<AboutPage isSmallScreen={isSmallScreen} />}
+              />
+              <Route path="*" element={<Navigate to="/home" replace />} />
+            </Routes>
+            <Footer isSmallScreen={isSmallScreen} />
+          </Box>
+        </Router>
+      </ThemeProvider>
     </LocalizationProvider>
   );
 };
